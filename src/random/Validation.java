263e0e5bb5c8709adfa9d73ce05d151e46ef5e31
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-03 14:29:08 -0700

    Validation

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -21,7 +21,19 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
-
+			if (randomNumber==0) {
+				System.out.println("Hello");
+			}else if (randomNumber==1) {
+				System.out.println("Con Mazola nunca estas sola");
+			}else if (randomNumber==2) {
+				System.out.println("Have a good day");
+			}else if (randomNumber==3) {
+				System.out.println("You are a good person");
+			}else if (randomNumber==4) {
+				System.out.println("You got this!");
+			}else if(randomNumber==5) {
+				System.out.println("Keep going foward");
+			}
 		// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
